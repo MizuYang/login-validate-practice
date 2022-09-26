e0b329f241554c6781b18369f11666182d0eb1d9
Mizu <psdk55052004@gmail.com>
2022-09-26 10:18:33 +0800

    新增 學生已登入頁面(成功、失敗回饋)

--- src/views/StudentView.vue
+++ src/views/StudentView.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <!-- 登入成功(需驗證通過才顯示) -->
+  <div v-if="validate">
+    <StudentInfo></StudentInfo>
+  </div>
+
+  <div v-else class="text-danger">
+    <h3>登入失敗</h3>
+    <router-link to="/login">點擊此處重新登入</router-link>
   </div>
 </template>
+
+<script>
+import StudentInfo from '../components/StudentInfo.vue'
+export default {
+  components: {
+    StudentInfo
+  },
+
+  data () {
+    return {
+      validate: false
+    }
+  },
+
+  watch: {
+  },
+
+  methods: {
+  },
+
+  mounted () {
+  }
+
+}
+</script>
+
+<style lang='scss' scope></style>
